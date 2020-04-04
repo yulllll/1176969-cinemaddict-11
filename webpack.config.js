172897a4a5fd8254a17c79c3a43a41f448679b6a
Vladimir <shopme13@yandex.ru>
2020-04-04 13:10:21 +0300

    Настроил сборку проекта: установил пакеты webpack и webpack-cli; добавил файл webpack.config.js; подключил файл сборки и добавил его в .gitignore; отредактировал файл .eslintrc.yml

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`, // Режим сборки
+  entry: `./src/main.js`, // Точка входа приложения
+  output: { // Настройка выходного файла
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map` // Подключаем карту исходников
+};
