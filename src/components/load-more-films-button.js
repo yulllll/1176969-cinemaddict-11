7453d0db298f047d49f03122f80fa9aaeabebd64
Vladimir <shopme13@yandex.ru>
2020-04-04 13:37:48 +0300

    Разбил код на модули

---
+++ src/components/load-more-films-button.js
@@ -0,0 +1,9 @@
+const createShowMoreFilmsTemplate = () => {
+  return (
+    `
+     <button class="films-list__show-more">Show more</button>
+    `
+  );
+};
+
+export {createShowMoreFilmsTemplate};
