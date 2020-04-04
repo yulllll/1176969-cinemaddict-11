90a0613c362a9556f99554a4d6e9109d0fa1f605
Vladimir <shopme13@yandex.ru>
2020-04-04 13:59:49 +0300

    Настроил локальный сервер для разработки

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  devtool: `source-map` // Подключаем карту исходников
+  devtool: `source-map`, // Подключаем карту исходников
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    watchContentBase: true
+  }
 };
