f6e13448f27d0513130ff6a5be0de3dc040d122a
Vladimir <shopme13@yandex.ru>
2020-04-29 01:43:36 +0300

    4.1.6 Исправит импорты, создаст три основные функции, выполнит рефакторинг

--- src/main.js
+++ src/main.js
@@ -1,78 +1,44 @@
-import {createUserProfileTemplate} from './components/user-profile.js';
-import {createNavigationTemplate} from './components/navigation.js';
-import {createSortTemplate} from './components/sort.js';
-import {createFilmsBlockTemplate} from './components/films-block.js';
-import {createFilmCardTemplate} from './components/film-card.js';
-import {createShowMoreFilmsTemplate} from './components/load-more-films-button.js';
-import {createFilmsListExtraTemplate} from './components/films-extra-list.js';
-import {createFilmDetailsPopupTemplate} from './components/films-details-popup.js';
-import {createMovieCommentsTemplate} from './components/movie-comments.js';
+// Импорт моков
 import {generateMovieCards} from './mock/film.js';
 import {generateFilters} from "./mock/filters.js";
+// Импорт компонентов
+import MovieCardComponent from "./components/movie-card.js";
+import MovieBlockComponent from "./components/movie-block.js";
+import MovieCommentsComponents from "./components/movie-comments.js";
+import MoviePopupComponent from "./components/movie-popup.js";
+import MovieExtraListComponent from "./components/movie-extra-list.js";
+import MovieLoadMoreButtonComponent from "./components/movie-loadmore-button.js";
+import MovieFiltersComponent from "./components/movie-filters.js";
+import MovieSortComponent from "./components/movie-sort.js";
+import UserProfileComponent from "./components/user-profile.js";
+import MovieBlockListComponent from "./components/movie-blocklist.js";
+// Импорт утилит
+import {RenderPosition, render, getIndexRatingCards} from "./utils.js";
 
-const FILMS_CARD_COUNT_DEFAULT = 1;
-const FILMS_CARD_COUNT = 25;
+// Констаты для параметров по умолчанию
+const FILMS_CARD_COUNT = 30;
 const FILMS_CARD_COUNT_MIN = 1;
 const SHOWING_MOVIE_CARDS_COUNT_ON_START = 5;
 const SHOWING_MOVIE_CARDS_COUNT_BY_BUTTON = 5;
 
+const FilmsExtraList = {
+  TOP_RATED: `Top rated`,
+  MOST_COMMENTED: `Most commented`,
+};
+
+// Генерируем моки
 const movieCards = generateMovieCards(FILMS_CARD_COUNT);
 const movieFilters = generateFilters(movieCards);
-// --------------------------------------- В консоле -----------------------------
-// console.log(movieCards);
-// --------------------------------------- В консоле -----------------------------
-const siteBodyElement = document.querySelector(`body`);
-const siteHeaderElement = siteBodyElement.querySelector(`.header`);
-const siteMainElement = siteBodyElement.querySelector(`.main`);
-
-const render = (container, template, count = FILMS_CARD_COUNT_DEFAULT, place = `beforeend`) => {
-  for (let i = 0; i < count; i++) {
-    container.insertAdjacentHTML(place, template);
-  }
-};
-
-render(siteHeaderElement, createUserProfileTemplate());
-render(siteMainElement, createNavigationTemplate(movieFilters));
-render(siteMainElement, createSortTemplate());
-render(siteMainElement, createFilmsBlockTemplate());
-
-const filmsBlockElement = siteMainElement.querySelector(`.films`);
-const filmsListElement = filmsBlockElement.querySelector(`.films-list`);
-const filmsContainerElement = filmsListElement.querySelector(`.films-list__container`);
-
-// Рендерим стартовую партию кинокарточек
-let showingMovieCardsCount = SHOWING_MOVIE_CARDS_COUNT_ON_START;
-movieCards.slice(0, showingMovieCardsCount)
-  .forEach((movieCard) => render(filmsContainerElement, createFilmCardTemplate(movieCard)));
-
 // Копируем массив основных кинокарточек для попапа
-let popupMovieCards = movieCards.slice(0, showingMovieCardsCount);
+let popupMovieCards = movieCards.slice(0, SHOWING_MOVIE_CARDS_COUNT_ON_START);
+// Массив для экстракарточек
+const extraMovieCards = [];
 
-// Рендерим кнопку показать еще
-render(filmsListElement, createShowMoreFilmsTemplate());
-const showMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
+const siteBodyElement = document.querySelector(`body`);
 
-const onShowMoreButtonClick = () => {
-  const prevMovieCardsCount = showingMovieCardsCount;
-  showingMovieCardsCount = showingMovieCardsCount + SHOWING_MOVIE_CARDS_COUNT_BY_BUTTON;
-
-  // Увеличиваем скопированный массив каждый раз при нажатии "показать больше"
-  popupMovieCards = movieCards.slice(0, showingMovieCardsCount);
-
-  movieCards.slice(prevMovieCardsCount, showingMovieCardsCount)
-    .forEach((movieCard) => render(filmsContainerElement, createFilmCardTemplate(movieCard)));
-
-  if (showingMovieCardsCount >= movieCards.length) {
-    showMoreButton.remove();
-    showMoreButton.removeEventListener(`click`, onShowMoreButtonClick);
-  }
-  initPopup();
-};
-showMoreButton.addEventListener(`click`, onShowMoreButtonClick);
-
-// Функция работы попапа
-const initPopup = () => {
-  // Находим все отредеренные карточки фильмов
+// Функция отрисовки попапа
+const renderPopup = () => {
+  // Находим все отрисованные карточки фильмов
   const movieCardsElement = document.querySelectorAll(`.film-card`);
 
   // Цикл по всем карточкам с индексом итерации и элементом массива для открытия попапа
@@ -116,20 +82,20 @@ const initPopup = () => {
 
     const onMovieCardClick = (evt) => {
       // Если клик по посту или названию или комментарию - ренедрим попап текущей карточки
-      if (evt.target === moviePoster || evt.target === movieTitle || evt.target === movieComment) {
+      const isMovieCardsElements = evt.target === moviePoster || evt.target === movieTitle || evt.target === movieComment;
+      if (isMovieCardsElements) {
         // Объединяем откртые при клике на "показать еще" и полученные экстаракарточки
         const allMovieCards = popupMovieCards.concat(extraMovieCards);
 
         render(siteBodyElement.querySelector(`.footer`),
-            createFilmDetailsPopupTemplate(allMovieCards[index]),
-            FILMS_CARD_COUNT_DEFAULT, `afterend`);
+            new MoviePopupComponent(allMovieCards[index]).getElement(), RenderPosition.AFTERBEGIN);
 
         const moviePopup = document.querySelector(`.film-details`);
         const commentsContentPopup = moviePopup.querySelector(`.form-details__bottom-container`);
         const commentsList = commentsContentPopup.querySelector(`.film-details__comments-list`);
 
         allMovieCards[index].comments.forEach((it) => {
-          render(commentsList, createMovieCommentsTemplate(it));
+          render(commentsList, new MovieCommentsComponents(it).getElement(), RenderPosition.BEFOREEND);
         });
         // Включаем функцию закрытия окна и передаём в неё попап
         initCloseButtonPopup(moviePopup);
@@ -139,99 +105,110 @@ const initPopup = () => {
     card.addEventListener(`click`, onMovieCardClick);
   }
 };
+// Функция отрисовки кинокарточек
+const renderMovieCards = (popup, movieComponent) => {
+  render(movieComponent.getElement(),
+      new MovieBlockListComponent().getElement(),
+      RenderPosition.BEFOREEND);
 
-// Массив для экстракарточек
-const extraMovieCards = [];
-// Если кинокарточки есть
-if (movieCards.length > FILMS_CARD_COUNT_MIN) {
-  // Получаем максимальное значение рейтинга и комментариев для экстракарточек
-  const getIndexRatingCards = (cards, propPath) => {
-    const startValue = [0];
-    const maxIndexes = [0];
+  const filmsListElement = siteBodyElement.querySelector(`.films-list`);
+  const filmsContainerElement = siteBodyElement.querySelector(`.films-list__container`);
 
-    const propNames = propPath.split(`.`);
+  // Рендерим стартовую партию кинокарточек
+  let showingMovieCardsCount = SHOWING_MOVIE_CARDS_COUNT_ON_START;
+  movieCards.slice(0, showingMovieCardsCount)
+    .forEach((movieCard) => render(filmsContainerElement,
+        new MovieCardComponent(movieCard).getElement(),
+        RenderPosition.BEFOREEND));
 
-    for (const [index, card] of cards.entries()) {
-      const prop = propNames.reduce((obj, name) => {
-        return obj[name];
-      }, card);
+  // Рендерим кнопку показать еще
+  render(filmsListElement, new MovieLoadMoreButtonComponent().getElement(), RenderPosition.BEFOREEND);
+  const showMoreButton = filmsListElement.querySelector(`.films-list__show-more`);
 
-      if (prop > startValue[startValue.length - 1]) {
-        startValue.push(prop);
-        maxIndexes.push(index);
+  const onShowMoreButtonClick = () => {
+    const prevMovieCardsCount = showingMovieCardsCount;
+    showingMovieCardsCount = showingMovieCardsCount + SHOWING_MOVIE_CARDS_COUNT_BY_BUTTON;
 
-        continue;
-      }
-      if (prop > startValue[startValue.length - 2]) {
-        startValue[startValue.length - 2] = prop;
-        maxIndexes[maxIndexes.length - 2] = index;
-      }
+    // Увеличиваем скопированный массив каждый раз при нажатии "показать еще"
+    popupMovieCards = movieCards.slice(0, showingMovieCardsCount);
+
+    movieCards.slice(prevMovieCardsCount, showingMovieCardsCount)
+      .forEach((movieCard) => render(filmsContainerElement,
+          new MovieCardComponent(movieCard).getElement(),
+          RenderPosition.BEFOREEND));
+
+    if (showingMovieCardsCount >= movieCards.length) {
+      showMoreButton.remove();
+      showMoreButton.removeEventListener(`click`, onShowMoreButtonClick);
     }
-
-    return {
-      maxIndex: maxIndexes.pop(),
-      nextIndex: maxIndexes.pop(),
-    };
+    popup();
   };
-  // Самые популярные фильмы
-  const popularsRatingsValue = getIndexRatingCards(movieCards, `rating`); // topRated
-  // Самые комментируемые фильмы
-  const popularsCommentsValue = getIndexRatingCards(movieCards, `comments.length`); // mostComments
-
+  showMoreButton.addEventListener(`click`, onShowMoreButtonClick);
+};
+// Функция отрисовки экстракарточек
+const renderExtraCards = (popup, movieComponent) => {
   // Рендерим разделы для экстракарточек
-  render(filmsBlockElement, createFilmsListExtraTemplate(`Top rated`));
-  render(filmsBlockElement, createFilmsListExtraTemplate(`Most commented`));
-
+  render(movieComponent.getElement(), new MovieExtraListComponent(FilmsExtraList.TOP_RATED).getElement(), RenderPosition.BEFOREEND);
+  render(movieComponent.getElement(), new MovieExtraListComponent(FilmsExtraList.MOST_COMMENTED).getElement(), RenderPosition.BEFOREEND);
   // Получаем список экстразделов
-  const filmsListExtraElement = filmsBlockElement.querySelectorAll(`.films-list--extra`);
-  // Рендерим карточки в экстраразделе и добавляем их в общий массив карточек
-  for (const [index, item] of filmsListExtraElement.entries()) {
-    const filmsExtraContainerElement = item.querySelector(`.films-list__container`);
-    // Если первый список (Top rated)
-    if (!index) {
-      render(filmsExtraContainerElement,
-          createFilmCardTemplate(movieCards[popularsRatingsValue.maxIndex]));
-      render(filmsExtraContainerElement,
-          createFilmCardTemplate(movieCards[popularsRatingsValue.nextIndex]));
+  const filmsListExtraElement = movieComponent.getElement().querySelectorAll(`.films-list--extra`);
 
-      extraMovieCards.push(movieCards[popularsRatingsValue.maxIndex]);
-      extraMovieCards.push(movieCards[popularsRatingsValue.nextIndex]);
-      // Если второй список (Most commented)
-    } else {
-      render(filmsExtraContainerElement,
-          createFilmCardTemplate(movieCards[popularsCommentsValue.maxIndex]));
-      render(filmsExtraContainerElement,
-          createFilmCardTemplate(movieCards[popularsCommentsValue.nextIndex]));
+  // Если кинокарточки есть
+  const isMovieCards = movieCards.length > FILMS_CARD_COUNT_MIN;
+  // Если кинокарточка одна
+  const isOneMovieCards = movieCards.length === FILMS_CARD_COUNT_MIN;
 
-      extraMovieCards.push(movieCards[popularsCommentsValue.maxIndex]);
-      extraMovieCards.push(movieCards[popularsCommentsValue.nextIndex]);
+  if (isMovieCards) {
+    // Самые популярные и комментируемые фильмы
+    const popularsRatingsValue = getIndexRatingCards(movieCards, `rating`);
+    const popularsCommentsValue = getIndexRatingCards(movieCards, `comments.length`);
+
+    // Рендерим карточки в экстраразделе и добавляем их в общий массив карточек
+    for (const [index, item] of filmsListExtraElement.entries()) {
+      const filmsExtraContainerElement = item.querySelector(`.films-list__container`);
+      // Если первый список (Top rated)
+      if (!index) {
+        render(filmsExtraContainerElement,
+            new MovieCardComponent(movieCards[popularsRatingsValue.maxIndex]).getElement(), RenderPosition.BEFOREEND);
+        render(filmsExtraContainerElement,
+            new MovieCardComponent(movieCards[popularsRatingsValue.nextIndex]).getElement(), RenderPosition.BEFOREEND);
+
+        extraMovieCards.push(movieCards[popularsRatingsValue.maxIndex]);
+        extraMovieCards.push(movieCards[popularsRatingsValue.nextIndex]);
+        // Если второй список (Most commented)
+      } else {
+        render(filmsExtraContainerElement,
+            new MovieCardComponent(movieCards[popularsCommentsValue.maxIndex]).getElement(), RenderPosition.BEFOREEND);
+        render(filmsExtraContainerElement,
+            new MovieCardComponent(movieCards[popularsCommentsValue.nextIndex]).getElement(), RenderPosition.BEFOREEND);
+
+        extraMovieCards.push(movieCards[popularsCommentsValue.maxIndex]);
+        extraMovieCards.push(movieCards[popularsCommentsValue.nextIndex]);
+      }
     }
-  }
-  initPopup();
-} else if (movieCards.length === FILMS_CARD_COUNT_MIN) {
-  // Рендерим разделы для экстракарточек
-  render(filmsBlockElement, createFilmsListExtraTemplate(`Top rated`));
-  render(filmsBlockElement, createFilmsListExtraTemplate(`Most commented`));
+    popup();
+  } else if (isOneMovieCards) {
+    // Рендерим карточки в экстраразделе и добавляем их в общий массив карточек
+    for (const item of filmsListExtraElement) {
+      const filmsExtraContainerElement = item.querySelector(`.films-list__container`);
 
-  // Получаем список экстразделов
-  const filmsListExtraElement = filmsBlockElement.querySelectorAll(`.films-list--extra`);
-
-  // Рендерим карточки в экстраразделе и добавляем их в общий массив карточек
-  for (const [index, item] of filmsListExtraElement.entries()) {
-    const filmsExtraContainerElement = item.querySelector(`.films-list__container`);
-
-    // Если первый список (Top rated)
-    if (!index) {
       render(filmsExtraContainerElement,
-          createFilmCardTemplate(movieCards[0]));
-      extraMovieCards.push(movieCards[0]);
-
-      // Если второй список (Most commented)
-    } else {
-      render(filmsExtraContainerElement,
-          createFilmCardTemplate(movieCards[0]));
+          new MovieCardComponent(movieCards[0]).getElement(), RenderPosition.BEFOREEND);
       extraMovieCards.push(movieCards[0]);
     }
+    popup();
   }
-  initPopup();
-}
+};
+
+const siteHeaderElement = siteBodyElement.querySelector(`.header`);
+const siteMainElement = siteBodyElement.querySelector(`.main`);
+
+render(siteHeaderElement, new UserProfileComponent().getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new MovieFiltersComponent(movieFilters).getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new MovieSortComponent().getElement(), RenderPosition.BEFOREEND);
+
+const movieComponent = new MovieBlockComponent();
+render(siteMainElement, movieComponent.getElement(), RenderPosition.BEFOREEND);
+
+renderMovieCards(renderPopup, movieComponent);
+renderExtraCards(renderPopup, movieComponent);
