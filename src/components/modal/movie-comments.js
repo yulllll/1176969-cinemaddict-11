74fc9eda1727fe746e46778442aa78caab91a9c9
Vladimir <shopme13@yandex.ru>
2020-05-24 22:22:25 +0300

    Установит библиотеку he

--- src/components/modal/movie-comments.js
+++ src/components/modal/movie-comments.js
@@ -1,6 +1,7 @@
 import {getCommentTime} from "../../utils/date.js";
 import {EMOTION_NAMES} from "../../const.js";
 import AbstractSmartComponent from "../abstract/abstract-smart";
+import {heEncode} from "he";
 
 const getUserCommentsTemplate = (movie) => {
   return movie.comments.map(({id, emotion, comment, author, date}) => {
@@ -12,7 +13,7 @@ const getUserCommentsTemplate = (movie) => {
                 <img src="${emotion}" width="55" height="55" alt="emoji-${emotion}">
               </span>
               <div>
-                <p class="film-details__comment-text">${comment}</p>
+                <p class="film-details__comment-text">${heEncode(comment)}</p>
                 <p class="film-details__comment-info">
                   <span class="film-details__comment-author">${author}</span>
                   <span class="film-details__comment-day">${commentsTime}</span>
